4d76940ba3fb1062cb5571c44067ff60424c128d
Reloy <Ruan_Eloy@gap.com>
2022-03-10 09:17:19 -0300

    create theme context

---
+++ src/context/theme/index.tsx
@@ -0,0 +1,36 @@
+import React, {createContext, useState} from 'react';
+
+enum ThemeVariants {
+  LIGHT,
+  DARK
+}
+
+type Theme = {
+  variant: ThemeVariants
+}
+
+const DEFAULT_VALUE = {
+  theme: {
+    variant: ThemeVariants.LIGHT,
+  },
+  setTheme: () => {}
+}
+
+type PropsThemeContext = {
+  theme: Theme,
+  setTheme: React.Dispatch<React.SetStateAction<Theme>>;
+}
+
+const ThemeContext = createContext<PropsThemeContext>(DEFAULT_VALUE);
+
+const ThemeContextProvider: React.FC = ({ children }) => {
+  const [theme, setTheme] = useState(DEFAULT_VALUE.theme)
+  return (
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export {ThemeContextProvider}
+export default ThemeContext
