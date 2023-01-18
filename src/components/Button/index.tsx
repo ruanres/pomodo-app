20154c41ae006a4ae89df88ffe3c215d64ccb731
Reloy <Ruan_Eloy@gap.com>
2023-01-18 15:19:21 -0300

    fix lint issues

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 type ButtonProps = {
   label: string;
   onClick: () => void;
-}
-const Button = ({label, onClick }: ButtonProps) => {
-  return <button onClick={onClick}>{label}</button>
-}
+};
+const Button = ({ label, onClick }: ButtonProps) => {
+  return <button onClick={onClick}>{label}</button>;
+};
 
 export default Button;
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -3,28 +3,28 @@ import Button from '../Button';
 import './timer.css';
 
 const formatToTen = (count: number) => {
-    return count < 10 ? `0${count}` : count;
-}
+  return count < 10 ? `0${count}` : count;
+};
 const getMinutes = (counter: number) => {
   const ONE_MINUTE = 60;
   const minutes = Math.floor(counter / ONE_MINUTE);
   const seconds = counter % ONE_MINUTE;
-  return `${formatToTen(minutes)}:${formatToTen(seconds)}`
-}
-type Status = 'start' | 'pause'
+  return `${formatToTen(minutes)}:${formatToTen(seconds)}`;
+};
+type Status = 'start' | 'pause';
 
 const Timer = () => {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
   const [status, setStatus] = useState<Status>('pause');
-  const intervalId = useRef(0)
+  const intervalId = useRef(0);
 
   useEffect(() => {
     if (status === 'start') {
       const id = window.setInterval(() => {
-        setCounter(current => current + 1);
+        setCounter((current) => current + 1);
       }, 1000);
       intervalId.current = id;
-    } else if(status === 'pause'){
+    } else if (status === 'pause') {
       window.clearInterval(intervalId.current);
     }
 
@@ -32,14 +32,14 @@ const Timer = () => {
   }, [status]);
 
   return (
-    <div className='timer'>
-      <div className='time'>{getMinutes(counter)}</div>
-      <div className='buttons'>
-        <Button label='work' onClick={() => setStatus('start')} />
-        <Button label='rest' onClick={() => setStatus('pause')} />
+    <div className="timer">
+      <div className="time">{getMinutes(counter)}</div>
+      <div className="buttons">
+        <Button label="work" onClick={() => setStatus('start')} />
+        <Button label="rest" onClick={() => setStatus('pause')} />
       </div>
     </div>
-    );
-}
+  );
+};
 
 export default Timer;
